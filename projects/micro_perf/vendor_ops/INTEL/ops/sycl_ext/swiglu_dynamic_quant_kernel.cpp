beb0dda582f50dd8d3ff4ace6fae67e05322f112
Tanner Voas <tanner.voas@intel.com>
2026-05-12 09:56:04 +0800

    V010804 - Sweep: target_wg=4, max_unroll=8

--- projects/micro_perf/vendor_ops/INTEL/ops/sycl_ext/swiglu_dynamic_quant_kernel.cpp
+++ projects/micro_perf/vendor_ops/INTEL/ops/sycl_ext/swiglu_dynamic_quant_kernel.cpp
@@ -173,7 +173,7 @@ void swiglu_dynamic_quant_impl(
                                   return launch_swiglu(unroll_tag, std::integral_constant<uint32_t, 131072>{}); // 32256 * 4 + 2048
     };
 
-    int target_wg = 2;
+    int target_wg = 4;
     int num_chunks = hidden_size / 64;
     int best_unroll = 1;
 
