0e1534cec6cd8b27c0a52c515e5fb62f3b2a65dc
Tanner Voas <tanner.voas@intel.com>
2026-05-12 14:32:40 +0800

    V02322048 - Sweep: target_total_threads=2048, max_unroll=32

--- projects/micro_perf/vendor_ops/INTEL/ops/sycl_ext/swiglu_dynamic_quant_kernel.cpp
+++ projects/micro_perf/vendor_ops/INTEL/ops/sycl_ext/swiglu_dynamic_quant_kernel.cpp
@@ -131,7 +131,7 @@ void swiglu_dynamic_quant_impl(
         });
     };
 
-    int target_total_threads = 1024;
+    int target_total_threads = 2048;
     auto is_valid_unroll = [&](int unroll) {
         int bs = unroll * 64;
         int max_wg_size = std::min(hidden_size / bs, 64);
